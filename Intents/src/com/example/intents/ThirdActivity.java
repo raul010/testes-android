b362eab8e4d015d44f45ab581d3dd2dab2c11fc8
Raul Leite <raul.abreu.leite@gmail.com>
2013-08-05 15:32:05 -0300

    Intents

---
+++ Intents/src/com/example/intents/ThirdActivity.java
@@ -0,0 +1,25 @@
+package com.example.intents;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ThirdActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.third);
+		
+		Bundle extras = getIntent().getExtras();
+		String name = extras.getString("name");
+		String age = extras.getString("age");
+		
+		
+		TextView nameTextView = (TextView) findViewById(R.id.name);
+		TextView ageTextView = (TextView) findViewById(R.id.age);
+		
+		nameTextView.setText(getString(R.string.user_name, name));
+		ageTextView.setText(getString(R.string.user_age, age));
+		
+	}
+}
